b16ac0d68fd04be7047a838ff63dd3ddbbde4dc7
Shivashankar-741 <shiva1894300@gmail.com>
2021-04-20 18:24:01 +0530

    changes

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -2,7 +2,7 @@ import { GraphQLServer, PubSub } from "graphql-yoga";
 import db from "./db";
 import Query from "./resolvers/Query";
 import Mutation from "./resolvers/Mutation";
-import Subscription from "./resolvers/Subscription"
+import Subscription from "./resolvers/Subscription";
 import Post from "./resolvers/Post";
 import User from "./resolvers/User";
 import Comment from "./resolvers/Comment";
